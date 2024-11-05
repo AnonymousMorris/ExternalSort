12684bb14f212891a9d6ae74aa1d6a46ed2e8723
AnonymousMorris <morrislin13@gmail.com>
2024-11-04 18:46:30 -0800

    downloaded sample data as well as wrote a test for controller

---
+++ src/ControllerTest.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+
+import student.TestCase;
+
+/**
+ * @author {Your Name Here}
+ * @version {Put Something Here}
+ */
+public class ControllerTest extends TestCase {
+    
+    
+    /**
+     * set up for tests
+     */
+    public void setUp() {
+        //nothing to set up.
+    }
+    
+    /**
+     * T
+     * @throws IOException 
+     */
+    public void testExternalsort() throws IOException {
+        String args = "data/sampleInput16_sorted.bin";
+        Controller controller = new Controller();
+        controller.run(args);
+    }
+
+}
+
+
